a1037cd4d7d85ad77955e4797cf13cfcbc4aea63
Laura Melges <melgesmoser.laura@gmail.com>
2026-01-30 00:36:13 -0300

    feat(home): add hero section to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Gradient1 from './assets/Gradient1.png'
 import './App.css'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <Hero />
       <img src={Gradient1} alt="Gradient" className="gradient" />
     </div>
   )
