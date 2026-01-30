615400d48c58657cb1964f44f0624c42e63ad851
Laura Melges <melgesmoser.laura@gmail.com>
2026-01-29 23:58:48 -0300

    feat: add responsive navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Gradient1 from './assets/Gradient1.png'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="app">
+      <NavBar />
       <img src={Gradient1} alt="Gradient" className="gradient" />
-      <h1 className="text">TEXTO DE TESTE</h1>
     </div>
   )
 }
